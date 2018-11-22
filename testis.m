b1b2d981a0d7cd2bde144b0381621f6f7c09f9d8
Molly Middagsfjell <mollymiddagsfjell@c-10-253-236-187.clients.eduroam.liu.se>
2018-11-22 10:41:51 +0100

    testis to master

---
+++ testis.m
@@ -0,0 +1,23 @@
+clear all
+clc
+
+a=imread('im1s.jpg'); 
+a = rgb2gray(a);
+bw = 1-imbinarize(a); 
+bw = imrotate(bw, 0.47);
+h = sum(bw,2);
+
+thresh = 300;
+h = bw > thresh;
+
+[pks, locs] = findpeaks(double(h));
+imshow(bw)
+
+hold on
+for i = 1:size(locs,1)
+   plot([1;size(bw,2)], [locs(i,1);locs(i,1)] , 'r');
+end
+
+hold on
+plot(h,1:size(h));
+
