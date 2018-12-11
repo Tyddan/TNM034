956e01cf5481e37059df95513ae0fd17940b1913
Tyddan <amanda.tyden@gmail.com>
2018-12-11 09:46:02 +0100

    Tar bort sextondelsnoter

--- createMask.m
+++ createMask.m
@@ -1,11 +1,9 @@
-function maskPicture = createMask(image ,median,stafflocs, pks)
+function mimage = createMask(image ,median,stafflocs, pks)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 
 %Constants
 k = 1;
-
-SE = strel('disk',floor(median/2));
 
 %Reshape and gett stafflines from the image
 %image = rgb2gray(image);
@@ -23,10 +21,6 @@ for i = 1:(length(pks)/5)
     k = k + 5;
 end
 
-%Add the pictures to the end result, maskPicture
-for a = 1:(length(pks)/5)
-    maskPicture{a} = imopen(mimage{a},SE);
-end
 
 
 end
---
+++ removeEightNotes.m
@@ -0,0 +1,20 @@
+function imageret = removeSixteennotes(median, originalimage)
+    SE = strel('line', median+3, 0);
+    image = imopen(originalimage,SE);
+    image = bwmorph(image,'open');
+    image = bwmorph(image,'thicken');
+    
+
+    [rows, columns] = size(image);
+    h = sum(image,1);
+
+    thresh = 8;
+    h = h > thresh;
+
+    [nrofsixth, locsixth] = findpeaks(double(h));
+    imageret = originalimage;
+    for i = 1:length(nrofsixth)
+        imageret(:,round(locsixth(i)-(1.1*median)):round(locsixth(i)+(2.1*median))) = 0;
+    end
+end
+
