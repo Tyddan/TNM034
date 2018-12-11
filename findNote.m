3e4fc10c92a58cc4c14783d494755a3c3f179e31
Molly Middagsfjell <mollymiddagsfjell@c-10-253-223-218.clients.eduroam.liu.se>
2018-12-11 16:20:56 +0100

    findNote

--- findNote.m
+++ findNote.m
@@ -1,10 +1,21 @@
-function notes = findNote(cent,lines)
+function notes = findNote(cent,lines,dividedImage, median)
     
     notes = "";
     for i = 1:length(cent)
         for j = 1:length(cent{i})
+            
+            % Index the note
             indexOfNote = nearestPoint(cent{1,i}(j,2),cell2mat(lines{1,i}(:,1)));
-            notes = [notes, cell2mat(lines{1,i}(indexOfNote,2))];
+            
+            % Check if sixteen or forth 
+           tOf = nameOfFunction(cent{1,i}(j,1),dividedImage{i},median);
+           
+           if tOf == 1
+               notes = [notes, cell2mat(lines{1,i}(indexOfNote,3))];
+           else 
+               notes = [notes, cell2mat(lines{1,i}(indexOfNote,2))];
+           end
+           
         end
         notes = [notes, 'n'];
     end
